7005a85e4ebe12bdb9a91682e110a88aea58e401
Patryk Błaziński <pblazinski12013@gmail.com>
2016-10-12 18:56:18 +0200

    Fixed compile error in msToMin pipe

--- src/app/msToMin.pipe.ts
+++ src/app/msToMin.pipe.ts
@@ -5,9 +5,11 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 
 export class MsToMinPipe implements PipeTransform {
-    transform(value: number): string {
-        let minutes = Math.floor(value / 60000);
-        let seconds = ((value % 60000) / 1000).toFixed(0);
-        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+
+    transform(value: any): string {
+        var min = Math.floor((value/1000/60) << 0);
+        var sec = Math.floor((value/1000) % 60);
+
+        return min + ":" + ((sec < 10) ? '0' : '') + sec;
     }
 }
