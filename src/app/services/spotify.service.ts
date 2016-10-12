6aa1b3e8c2e0bfcdb1d4bc25a83ab5a852c2446c
Patryk Błaziński <pblazinski12013@gmail.com>
2016-10-12 15:06:26 +0200

    Add spotify service

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import "rxjs/add/operator/map";
+/**
+ * Created by pblazinski on 11.10.16.
+ */
+
+@Injectable()
+export class SpotifyService {
+
+    constructor(private http: Http) {
+    }
+
+    search(q: string, type: string) {
+        return this.http.get("https://api.spotify.com/v1/search?q=" + q + "&offset=0&limit=10&type=" + type)
+            .map(res=> res.json());
+    }
+
+}
